9fd030f3c93e6debff8c19264481f74efae8c522
Nikita Mitiagin <nikita.mitiagin@gmail.com>
2024-04-17 20:43:50 +0400

    Fixed id for new rates document saving. Removed stub readme.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { Client, Databases, Query } from "node-appwrite";
+import axios from "axios";
+import { Client, Databases, Query, ID } from "node-appwrite";
 
 export default async function fetchAndSaveRates(context) {
     try {
@@ -9,42 +9,65 @@ export default async function fetchAndSaveRates(context) {
         client.setProject(process.env.PROJECT_ID);
         client.setKey(process.env.APPWRITE_API_KEY);
 
-        const response = await axios.get(`https://api.currencyapi.com/v3/latest?apikey=${process.env.RATES_API_KEY}`);
+        const response = await axios.get(
+            `https://api.currencyapi.com/v3/latest?apikey=${process.env.RATES_API_KEY}`
+        );
         const rates = response?.data?.data ?? {};
 
         console.log("Retrieved rates", rates);
 
-        const ratesArray = Object.keys(rates).map(key => [key, rates[key].value]);
-        const DDMMYYYY = new Date().toLocaleDateString("en-GB").replace(/\//g, "");
+        const ratesArray = Object.keys(rates).map((key) => [
+            key,
+            rates[key].value,
+        ]);
+        const DDMMYYYY = new Date()
+            .toLocaleDateString("en-GB")
+            .replace(/\//g, "");
 
         // Check if a document with this date already exists
         let searchResponse = await database.listDocuments(
             process.env.DATABASE_ID,
             process.env.COLLECTION_ID,
-            [
-                Query.equal("date", DDMMYYYY)
-            ]
+            [Query.equal("date", DDMMYYYY)]
         );
 
-        let documentId = searchResponse.documents.length > 0 ? searchResponse.documents[0].$id : null;
+        let documentId =
+            searchResponse.documents.length > 0
+                ? searchResponse.documents[0].$id
+                : null;
 
         const document = {
             date: DDMMYYYY,
-            jsonRates: JSON.stringify(ratesArray)
+            jsonRates: JSON.stringify(ratesArray),
         };
         if (documentId) {
             console.log("Updating existing:", documentId, document);
             // Update the existing document
-            await database.updateDocument(process.env.DATABASE_ID, process.env.COLLECTION_ID, documentId, document);
+            await database.updateDocument(
+                process.env.DATABASE_ID,
+                process.env.COLLECTION_ID,
+                documentId,
+                document
+            );
         } else {
+            documentId = ID.unique();
             console.log("Saving new:", documentId, document);
             // Create a new document
-            await database.createDocument(process.env.DATABASE_ID, process.env.COLLECTION_ID, documentId, document);
+            await database.createDocument(
+                process.env.DATABASE_ID,
+                process.env.COLLECTION_ID,
+                documentId,
+                document
+            );
         }
         console.log("Done:", documentId);
-        return context?.res ? context.res.json({ ok: true, rates: rates }) : undefined;
+        return context?.res
+            ? context.res.json({ ok: true, rates: rates })
+            : undefined;
     } catch (error) {
         console.error("Error fetching or saving rates:", error);
-        return context?.res ? context.res.json({ ok: false, error: error }) : undefined;
+        return context?.res
+            ? context.res.json({ ok: false, error: error })
+            : undefined;
     }
 }
